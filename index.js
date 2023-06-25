a47f7ed4ac626179458caee1b9dc25ace1edf9a0
sakshi_13 <sakshimalviya098@gmail.com>
2023-06-25 21:12:49 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
